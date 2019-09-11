67ff606addce5777769873e8773737418e76aae9
jacobhanson <jhanreg11@gmail.com>
2019-09-11 00:25:35 -0700

    working on mobile updates

--- website/js/index.js
+++ website/js/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
   introScreen();
+  $('#side-click').click(openNav())
+  $('.closebtn').click(closeNav())
   setInterval(showFunFact, 10000)
 });
 
@@ -68,16 +70,16 @@ function showFunFact() {
     var x1 = $div1.offset().left;
     var y1 = $div1.offset().top;
     var h1 = $div1.outerHeight(true);
-    var w1 = $div1.outerWidth(true);      
+    var w1 = $div1.outerWidth(true);
     var b1 = y1 + h1;
     var r1 = x1 + w1;
     var x2 = $div2.offset().left;
     var y2 = $div2.offset().top;
     var h2 = $div2.outerHeight(true);
-    var w2 = $div2.outerWidth(true);      
+    var w2 = $div2.outerWidth(true);
     var b2 = y2 + h2;
     var r2 = x2 + w2;
-        
+
     if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
       return true;
   }
@@ -109,4 +111,14 @@ function showFunFact() {
     setHTML('#fact-title', '#fact-body')
     $fact.fadeIn(5000)
   })
+}
+
+
+function openNav() {
+  console.log('side-clicked')
+  $('#side-nav').width("250px");
+}
+
+function closeNav() {
+  $('#side-nav').width("0");
 }
