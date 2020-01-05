94d3117f7f00d669f3fb0c4b0f96da4eef2c532c
jacobhanson <jhanreg11@gmail.com>
2020-01-05 14:20:58 -0800

    update about

--- new-site/src/pages/about-page/about-page.js
+++ new-site/src/pages/about-page/about-page.js
@@ -7,8 +7,7 @@ import Reveal from 'react-reveal/Reveal'
 const AboutPage = (props) => (
 	<Section title="About Me" id="about">
 			<div className="body-text">Hi, I'm Jacob Hanson-Regalado. I am a sophomore studying Computer Science at Diablo
-				Valley College in Pleasant Hill, California. I have a passion for innovation and technology, and I have a thirst
-				for knowledge. I am always expanding my skills and exploring new interests. Most of my experience outside of the
+				Valley College in Pleasant Hill, California. I have a passion for innovation, and a thirst for knowledge. I am always expanding my skills and exploring new interests. Most of my experience outside of the
 				classroom up to this point has been web-development, but I am also interested in exploring other more advanced
 				subjects like Artificial Intelligence and Machine Learning. I also enjoy reading, eating, hanging out with my
 				girlfriend, and playing spike ball with my friends. I hope you enjoy my site!</div>
