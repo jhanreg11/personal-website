291a198ed3d155986c5c146db338d2f6fdeebab8
jacobhanson <jhanreg11@gmail.com>
2020-01-06 23:12:24 -0800

    creating build

---
+++ website/build/precache-manifest.b7b171a3f8bf9c1000f264133822249c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "061e574a34e06edf7271cb86022cdbbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca6212a4fb095f7a53d8",
+    "url": "/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "586b637eecbf635da6f5",
+    "url": "/static/css/main.6f92de35.chunk.css"
+  },
+  {
+    "revision": "ca6212a4fb095f7a53d8",
+    "url": "/static/js/2.03af643f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.03af643f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "586b637eecbf635da6f5",
+    "url": "/static/js/main.f025d89f.chunk.js"
+  },
+  {
+    "revision": "2f5532d4aa22486ffd78",
+    "url": "/static/js/runtime-main.7b5a8d85.js"
+  },
+  {
+    "revision": "29c92e824097f9aeaf24f699f4472ef4",
+    "url": "/static/media/POLYA.29c92e82.woff"
+  },
+  {
+    "revision": "57ea44bb3dd9c1a3d6aace37d7810526",
+    "url": "/static/media/POLYA.57ea44bb.otf"
+  },
+  {
+    "revision": "66c022ea0100695266b2ba9be87bcc54",
+    "url": "/static/media/POLYA.66c022ea.svg"
+  },
+  {
+    "revision": "f57cd3a512d934f319523ae1629efa65",
+    "url": "/static/media/POLYA.f57cd3a5.eot"
+  },
+  {
+    "revision": "24b172be71c2390a06168651eab47ed1",
+    "url": "/static/media/cashout_screenshot.24b172be.png"
+  },
+  {
+    "revision": "0d641c554272f3f1ec5db9fe9e5a5141",
+    "url": "/static/media/collage.0d641c55.jpg"
+  },
+  {
+    "revision": "9ce68bcab87f651b5a6bd16489896ef7",
+    "url": "/static/media/github_icon.9ce68bca.png"
+  },
+  {
+    "revision": "e1f2dc064fe0f1e60a74f89794bd89da",
+    "url": "/static/media/ig_icon.e1f2dc06.png"
+  },
+  {
+    "revision": "ab3e617878175f7f5f15fdccfb2a6eb6",
+    "url": "/static/media/neurocars-screenshot.ab3e6178.png"
+  },
+  {
+    "revision": "347c5b7772b06c514dfb5edbb8fe5bdd",
+    "url": "/static/media/trackIt_screenshot.347c5b77.png"
+  },
+  {
+    "revision": "56bf037c7207416777ad41a73b6d2e3a",
+    "url": "/static/media/twitter_icon.56bf037c.png"
+  }
+]);
